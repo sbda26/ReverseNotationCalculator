fb2f0932278703c5d37ac97fdc576d1b6e68b979
sbda26 <46138243+sbda26@users.noreply.github.com>
2022-01-22 12:23:32 -0500

    Added new operations and changed error handling.

--- RNC.Library/CalculateClass.cs
+++ RNC.Library/CalculateClass.cs
@@ -6,41 +6,54 @@ namespace RNC.Library
 {
     public class CalculateClass
     {
+        private readonly List<string> _SingleOperandOperators;
         private readonly List<string> _BitwiseOperators;
         private readonly List<string> _AllValidOperators;
 
         public CalculateClass()
         {
+            _SingleOperandOperators = new List<string> { "!", "sqrt", "cbrt", "log", "log2", "log10", "logB" };
             _BitwiseOperators = new List<string> { "|", "&", "^" };
-            _AllValidOperators = new List<string> { "+", "-", "*", "/", "÷", "**" };
+            _AllValidOperators = new List<string> { "+", "-", "*", "/", "÷", "**", "%" };
+            _AllValidOperators.AddRange(_SingleOperandOperators);
             _AllValidOperators.AddRange(_BitwiseOperators);
         }
 
         public double? Calculate(ReverseNotationCalculatorClass rnc)
         {
+            string value1 = rnc.Value1 != null ? rnc.Value1.ToString() : null;
+            string value2 = rnc.Value2 != null ? rnc.Value2.ToString() : null;
+
             if (IsValidOperation(rnc.Operation) == true)
             {
-                if (IsNumeric(rnc.Value1) == true)
+                if (IsNumeric(value1) == true)
                 {
-                    if (IsNumeric(rnc.Value2) == true)
+                    if (IsSingleOperandOperation(rnc) == true)
+                    {
+                        if (rnc.Value2 != null)
+                            throw RNCArgumentException(value2, "Value2", nonNullValue2Error: true);
+                        else
+                            return SingleOperandOperation(value1, rnc.Operation);
+                    }
+                    else if (IsNumeric(value2) == true)
                     {
                         if (IsBitwiseOperation(rnc.Operation) == false)
                             return NonBitwiseOperation(rnc);
                         else
                         {
-                            if (IsFloatingPoint(rnc.Value1) == true)
-                                throw RNCArgumentException(rnc.Value1, "Value1", true);
-                            else if (IsFloatingPoint(rnc.Value2) == true)
-                                throw RNCArgumentException(rnc.Value2, "Value2", true);
+                            if (IsFloatingPoint(value1) == true)
+                                throw RNCArgumentException(value1, "Value1", floatingPointError: true);
+                            else if (IsFloatingPoint(value2) == true)
+                                throw RNCArgumentException(value2, "Value2", floatingPointError: true);
                             else
                                 return BitwiseOperation(rnc);
                         }
                     }
                     else
-                        throw RNCArgumentException(rnc.Value2, "Value2", false);
+                        throw RNCArgumentException(value2, "Value2");
                 }
                 else
-                    throw RNCArgumentException(rnc.Value1, "Value1", false);
+                    throw RNCArgumentException(value1, "Value1");
             }
             else
                 throw new InvalidOperationException("Operator symbol not valid.");
@@ -48,20 +61,20 @@ namespace RNC.Library
 
         private bool IsValidOperation(string operation) => _AllValidOperators.Contains(operation);
 
-        private bool IsNumeric(object value) => double.TryParse(value.ToString(), out _);
+        private bool IsNumeric(string value) => double.TryParse(value, out _);
 
         private bool IsBitwiseOperation(string operation) => _BitwiseOperators.Contains(operation);
 
-        private bool IsFloatingPoint(object value)
-        {
-            string temp = value.ToString();
-            return temp.Contains(".");
-        }
+        private bool IsFloatingPoint(string value) => double.TryParse(value, out _) == true && long.TryParse(value, out _) == false;
+
+        private bool IsSingleOperandOperation(ReverseNotationCalculatorClass rnc) => _SingleOperandOperators.Contains(rnc.Operation) == true;
+
+        private bool IsNegative(string value) => value != null && (value.StartsWith("-") || value.EndsWith("-"));
 
         private double? BitwiseOperation(ReverseNotationCalculatorClass rnc)
         {
-            long? v1 = Convert.ToInt64(rnc.Value1.ToString());
-            long? v2 = Convert.ToInt64(rnc.Value2.ToString());
+            long? v1 = Convert.ToInt64(rnc.Value1);
+            long? v2 = Convert.ToInt64(rnc.Value2);
 
             switch (rnc.Operation)
             {
@@ -92,24 +105,64 @@ namespace RNC.Library
                     }
                 case "**":
                     return Math.Pow(v1.Value, v2.Value);
+                case "%":
+                    return v1 % v2;
                 default:
                     return null;
             }
         }
 
-        private Exception RNCArgumentException(object value, string valueName, bool floatingPointError)
+        private double? SingleOperandOperation(string value1, string operation)
         {
-            if (value != null)
+            switch(operation)
+            {
+                case "!":
+                    {
+                        if (IsFloatingPoint(value1) == true)
+                            throw RNCArgumentException(value1, "Value1", floatingPointError: true);
+                        else if (IsNegative(value1) == true)
+                            throw RNCArgumentException(value1, "Value1", negativeValueError: true);
+                        else
+                        {
+                            ulong value = Convert.ToUInt64(value1);
+                            for (ulong index = (value - 1); index > 1; index--)
+                                value *= index;
+                            return value;
+                        }
+                    }
+                case "sqrt": return Math.Sqrt(Convert.ToDouble(value1));
+                case "cbrt": return Math.Cbrt(Convert.ToDouble(value1));
+                case "log": return Math.Log(Convert.ToDouble(value1));
+                case "log2": return Math.Log2(Convert.ToDouble(value1));
+                case "log10": return Math.Log10(Convert.ToDouble(value1));
+                case "logB": return Math.ILogB(Convert.ToDouble(value1));
+                default: throw new NotImplementedException();
+            }
+        }
+
+        private Exception RNCArgumentException(string value, string valueName,
+                                               bool floatingPointError = false, bool negativeValueError = false, bool nonNullValue2Error = false)
+        {
+            if (value == null)
+                throw new ArgumentNullException("Value cannot be null.");
+            else
             {
                 var msg = new StringBuilder($"Invalid argument ({value.ToString()}) for [{valueName}].");
 
-                if (floatingPointError == true)
-                    msg.Append(" For a bitwise operation, both values must be integers.");
+                if (!floatingPointError && !negativeValueError && !nonNullValue2Error)
+                    return new ArgumentException($"Invalid argument ({value.ToString()}) for [{valueName}].");
+                else
+                {
+                    if (floatingPointError)
+                        msg.AppendLine("All values must be integers for this operation.");
+                    if (negativeValueError)
+                        msg.AppendLine("Value cannot be negative.");
+                    if (nonNullValue2Error)
+                        msg.AppendLine("Value2 must be null. Only one value is allowed.");
 
-                return new ArgumentException($"Invalid argument ({value.ToString()}) for [{valueName}].");
+                    return new ArgumentException(msg.ToString());
+                }
             }
-            else
-                return new ArgumentNullException($"[{valueName}] cannot be null.");
         }
     }
 }
--- RNCAPI/Controllers/RNCController.cs
+++ RNCAPI/Controllers/RNCController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RNC.Library;
 using System;
 using System.Collections.Generic;
